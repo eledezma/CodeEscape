967d08345c8ee82842b5acbac9ecbd336ae7c033
AllTimePlumber <superbros2@gmail.com>
2014-09-27 14:45:53 -0700

    Button
    Added Button files to Room Stage 0

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ButtonTrigger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTrigger : MonoBehaviour {
+
+	public GameObject button;
+	public GameObject door; //Abel's Door
+	public AnimationClip PushDown;
+	public AnimationClip PushUp;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator OnTriggerEnter (Collider other){
+		
+		if (other.gameObject.tag == "Player"){
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			Debug.Log("Object Entered the trigger");
+			button.animation.Play (PushDown.name);
+		}
+		
+		
+	}
+
+	IEnumerator OnTriggerExit (Collider other){
+		
+		if (other.gameObject.tag == "Player"){
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			Debug.Log("Object Exited the trigger");	
+			button.animation.Play (PushUp.name);
+		}
+	}	
+}
+
