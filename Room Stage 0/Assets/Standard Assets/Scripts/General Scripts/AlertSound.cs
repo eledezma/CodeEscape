61ec3a71d6a99d773d51a1efd2e9890b812a4df1
daniodicho <dannykaka2009@hotmail.com>
2014-10-27 23:55:19 -0700

    object details, script only

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/AlertSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlertSound : MonoBehaviour {
+	bool played = false;
+	public AudioClip alert;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fYRot = Camera.main.transform.eulerAngles.y;
+		if ((fYRot < 10)&&(!played)) {
+			audio.PlayOneShot (alert);
+			played = true;
+		}
+	}
+}
--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ToolTipTxt.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ToolTipTxt.cs
@@ -4,11 +4,14 @@ using System.Collections;
 
 public class ToolTipTxt : MonoBehaviour {
 	public string toolTipText= "";
+	public string info = "";
 	
 	private string currentToolTipText= "";
 	private GUIStyle guiStyleFore;
 	private GUIStyle guiStyleBack;
 	private bool nextTo = false;
+	bool clickable = false;
+	bool enabled = false;
 	
 	void  Start (){
 		guiStyleFore = new GUIStyle();
@@ -25,20 +28,38 @@ public class ToolTipTxt : MonoBehaviour {
 		guiStyleBack.fontStyle = FontStyle.Italic;
 		guiStyleBack.fontSize = 18;
 	}
+
+	void Update(){
+				if (Input.GetMouseButtonDown (0)) {
+						if (clickable && !enabled) {
+								enabled = true;
+								Screen.lockCursor = false;
+
+						} else
+								enabled = false;
+				}
+	}
 	void OnTriggerEnter(){
 		nextTo = true;
 	}
 	void  OnMouseEnter (){
 		currentToolTipText = toolTipText;
-
+		clickable = true;
 	}
 	
 	void  OnMouseExit (){
 		currentToolTipText = "";
+		clickable = false;
+	//	enabled = false;
 	}
 	
 	void  OnGUI (){
-		if (currentToolTipText != "")
+		GUI.UnfocusWindow ();
+		if ((enabled)&&(info!="")) {
+			GUI.Box(new Rect (Screen.width*0.35f,Screen.height*0.35f,Screen.width*0.3f,Screen.height*0.3f),currentToolTipText);
+		    GUI.Label(new Rect (Screen.width*0.35f,Screen.height*0.35f,Screen.width*0.3f,Screen.height*0.3f),"\n\n"+info);
+		}
+		else if (currentToolTipText != "")
 		{
 			float x= Event.current.mousePosition.x;
 			float y= Event.current.mousePosition.y;
