02baa99cb987c2ab8357adb20b658f9ed8cd71e7
AllTimePlumber <superbros2@gmail.com>
2014-10-29 14:38:39 -0700

    Updated Fake Stage 3
    Added Move Block script and some blocks to the scene

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/MoveBlock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock : MonoBehaviour {
+
+	public int height;
+	float initialHeight = -3.029409f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newHeight = initialHeight + (7 * height);
+		transform.position = new Vector3(transform.position.x, newHeight, transform.position.z);
+	}
+}
