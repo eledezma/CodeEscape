c00201d574668b9bdc2cd13e203a1c61f94c7765
Ernie Ledezma <eledezma518@gmail.com>
2014-11-04 23:03:10 -0800

    Added scary door to pathway, added few tool tips
    There is a scary door after you finish level one that when approached
    plays a sound file.  Its a torture room…

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/TortureRoom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TortureRoom : MonoBehaviour {
+
+	public AudioClip tortureSound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.gameObject.name == "creepy_door")
+		{
+			audio.PlayOneShot(tortureSound, 25);
+		}
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
