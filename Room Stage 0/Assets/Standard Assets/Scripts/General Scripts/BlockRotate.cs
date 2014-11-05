0806a597747202ac84145a4b0c4b756da8eb8819
AllTimePlumber <superbros2@gmail.com>
2014-11-04 20:59:04 -0800

    Stage 4 Setup
    Made a scene for Stage 4 (copy for Stage 2 for now). Added dice blocks
    and button.

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/BlockRotate.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockRotate : MonoBehaviour {
+
+	public bool rot;
+	public bool delay;
+	public bool loaded;
+	private bool wasLoaded;
+	public GameObject dice;
+	// Use this for initialization
+
+	void Start(){
+		wasLoaded = false;
+	}
+	
+	void Update(){
+		if(rot) {
+			RandomTime ();
+		}
+		
+		if (delay){
+			StartCoroutine (DelayTime ());
+			delay = false;
+		}
+
+	}
+	
+	void RandomTime()
+	{
+		//yield return new WaitForSeconds (10f);
+		int sw = Random.Range (1, 5);
+		switch (sw)
+		{
+		case 1: 
+			transform.Rotate(new Vector3 (90, 0, 0));
+			break;
+		case 2: 
+			transform.Rotate(new Vector3 (-90, 0, 0));
+			break;
+		case 3: 
+			transform.Rotate(new Vector3 (0, 90, 0));
+			break;
+		case 4: 
+			transform.Rotate(new Vector3 (0, -90, 0));
+			break;
+		}
+		
+		
+	}
+	
+	IEnumerator DelayTime(){
+		yield return new WaitForSeconds (5f);
+		rot = false;
+		if (loaded)
+			StartCoroutine (Get6 ());
+		else if (!loaded && wasLoaded)
+			GameObject.Find ("ButtonTrigger").GetComponent<ButtonTriggerStage4> ().PushItUp ();
+	}
+	
+	
+	IEnumerator Get6(){	
+		while (dice.GetComponent<Die_d6>().value != 6) {
+			int sw = Random.Range (1, 5);
+			switch (sw) {
+			case 1: 
+				transform.Rotate (new Vector3 (90, 0, 0));
+				break;
+			case 2: 
+				transform.Rotate (new Vector3 (-90, 0, 0));
+				break;
+			case 3: 
+				transform.Rotate (new Vector3 (0, 90, 0));
+				break;
+			case 4: 
+				transform.Rotate (new Vector3 (0, -90, 0));
+				break;
+			}
+			
+			yield return new WaitForSeconds (0.5f);
+			Debug.Log("Wait for 0.5 seconds");
+			
+		}
+		wasLoaded = true;
+		GameObject.Find ("ButtonTrigger").GetComponent<ButtonTriggerStage4> ().PushItUp ();
+
+	}
+}
---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ButtonTriggerStage4.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTriggerStage4 : MonoBehaviour {
+
+	public AudioClip ButtonClick;
+	public GameObject button;
+	//public GameObject door; //Abel's Door
+	public AnimationClip PushDown;
+	public AnimationClip PushUp;
+	//public bool k;
+	private bool buttondown;
+	
+	// Use this for initialization
+	void Start () {
+		buttondown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (k)
+			button.animation.Play (PushDown.name);*/
+	}
+
+	IEnumerator OnTriggerEnter (Collider other){
+		
+		if (other.gameObject.tag == "Player" && !buttondown){
+			buttondown = true;
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			Debug.Log("Object Entered the trigger");
+			audio.PlayOneShot(ButtonClick);
+			button.animation.Play (PushDown.name);
+			GameObject.Find ("d67").GetComponent<BlockRotate>().rot = true;
+			GameObject.Find ("d67").GetComponent<BlockRotate>().delay = true;
+			GameObject.Find ("d6").GetComponent<BlockRotate>().rot = true;
+			GameObject.Find ("d6").GetComponent<BlockRotate>().delay = true;
+			//yield return new WaitForSeconds (PushDown.length);
+		}
+		
+		
+	}
+
+	public void PushItUp(){
+		button.animation.Play (PushUp.name);
+		Debug.Log ("PUSH ME UP");
+		buttondown = false;
+
+
+	}
+
+	IEnumerator OnTriggerStay (Collider other){
+		if (other.gameObject.tag == "Player" && !buttondown){
+			buttondown = true;
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			Debug.Log("Object Stayed in the trigger");
+			audio.PlayOneShot(ButtonClick);
+			button.animation.Play (PushDown.name);
+			GameObject.Find ("d67").GetComponent<BlockRotate>().rot = true;
+			GameObject.Find ("d67").GetComponent<BlockRotate>().delay = true;
+			GameObject.Find ("d6").GetComponent<BlockRotate>().rot = true;
+			GameObject.Find ("d6").GetComponent<BlockRotate>().delay = true;
+			//yield return new WaitForSeconds (PushDown.length);
+		}
+
+	}
+
+	/*IEnumerator OnTriggerExit (Collider other){
+		
+		if (other.gameObject.tag == "Player"){
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			//Debug.Log("Object Exited the trigger");	
+			button.animation.Play (PushUp.name);
+		}
+	}*/
+
+
+}
