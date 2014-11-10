f16a427a98a8cb3129321214a832d8b5a766b3e7
Ernie Ledezma <eledezma518@gmail.com>
2014-11-10 10:45:38 -0800

    Added Timer to Stage 4

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	
+	
+	float timer = 10.0f;
+	
+	
+	void  Update (){
+		
+		timer -= Time.deltaTime;
+		
+		if(timer <=0){
+			
+			timer = 0;
+			//DO SOMETHING KOOL
+		}
+		
+	}
+	
+	void  OnGUI (){
+		
+		GUI.Box(new Rect(650, 40, 80, 40), "" + timer.ToString("0"));
+	}
+
+}
