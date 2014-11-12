91db8e2ead89617d2c5201c9c082d243bb22c963
818headbanger <josepech818@gmail.com>
2014-11-12 11:14:37 -0800

    Script for Scanner
    Added Scanner Script

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ScannerInfo.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScannerInfo : MonoBehaviour
+{
+	public bool guiEnabeled = true;
+	public string info = "The Scanner class is part of the java.util package." +
+		"\nScanner's in java are used to get user input, but" +
+		"\n that is not their only purpose. The method nextLine()" +
+		"\n is part of the Scanner class and is used to get the " +
+		"\n next line the user enters as the input.";
+		// Use this for initialization
+		void Start (){
+		GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled=false;
+		GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled=false;
+		GameObject.Find("Initialization").GetComponent<CursorTime>().showCursor=false;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	public void OnGUI(){
+		if (guiEnabeled) {
+			GUILayout.Box("Info");
+			GUILayout.TextArea(info);
+			//if(GUILayout.Button("Submit")){
+			if(Input.GetKeyDown("e")){
+				resume();
+				//CursorTime.showCursor = false;
+			}
+		}
+	}
+	public void resume(){
+		Time.timeScale = 1.0f;
+		guiEnabeled = false;
+		GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled=true;
+		GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled=true;
+		GameObject.Find("Initialization").GetComponent<CursorTime>().showCursor=true;
+	}
+}
+
