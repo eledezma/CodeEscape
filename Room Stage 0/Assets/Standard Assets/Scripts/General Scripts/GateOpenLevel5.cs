7ad1f58f2dae8cb6156a1b937eb68c67979c261d
AllTimePlumber <superbros2@gmail.com>
2015-01-26 17:23:15 -0800

    Door/Gate Animation Level 5
    added Door and Gate animations. Press d for door and g for gate

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/GateOpenLevel5.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateOpenLevel5 : MonoBehaviour {
+
+	public AnimationClip gateClosed;
+	public bool lowered = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lowered || Input.GetKeyDown ("g")){
+			animation.Play (gateClosed.name);
+			lowered = false;
+		}
+	}
+}
