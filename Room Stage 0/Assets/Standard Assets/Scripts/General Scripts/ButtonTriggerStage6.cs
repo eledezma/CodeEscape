d34dcc64cf62418481b470c7e43c08aac7662502
AllTimePlumber <superbros2@gmail.com>
2015-02-09 19:29:47 -0800

    Turret Movement + Buttons Level 6
    There is now a Reset and Play Button that turns the turret around.

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ButtonTriggerStage6.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTriggerStage6 : MonoBehaviour {
+
+	public AudioClip ButtonClick;
+	public GameObject button;
+	public GameObject turret;
+	public AnimationClip PushDown;
+	public AnimationClip PushUp;
+	public bool buttondown;
+	string objectName;
+	
+	// Use this for initialization
+	void Start () {
+		buttondown = false;
+		objectName = this.gameObject.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {;
+	}
+	
+	IEnumerator OnTriggerEnter (Collider other){
+		
+		if (other.gameObject.tag == "Player" && !buttondown){
+			buttondown = true;
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			audio.PlayOneShot(ButtonClick);
+			if (objectName == "ResetTrigger")
+				turret.GetComponent<TurretTurn>().reset = true;
+			else
+				turret.GetComponent<TurretTurn>().turn = true;
+			button.animation.Play (PushDown.name);
+			yield return new WaitForSeconds (1f);
+			button.animation.Play (PushUp.name);
+			buttondown = false;
+			yield return new WaitForSeconds (1f);
+		}	
+	}
+	
+	IEnumerator OnTriggerStay (Collider other){
+		if (other.gameObject.tag == "Player" && !buttondown){
+			buttondown = true;
+			if (button.animation.isPlaying)
+				yield return new WaitForSeconds (0.5f);
+			audio.PlayOneShot(ButtonClick);
+			if (objectName == "ResetTrigger")
+				turret.GetComponent<TurretTurn>().reset = true;
+			else
+				turret.GetComponent<TurretTurn>().turn = true;
+			button.animation.Play (PushDown.name);
+			yield return new WaitForSeconds (1f);
+			button.animation.Play (PushUp.name);
+			buttondown = false;
+			yield return new WaitForSeconds (1f);
+		}
+		
+	}
+}
---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/TurretTurn.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretTurn : MonoBehaviour {
+
+	public AnimationClip turn1;
+	public AnimationClip turn2;
+	public AnimationClip turn3;
+	public AnimationClip reverseTurn;
+	public bool reset;
+	public bool turn;
+	int pos;
+	
+	// Use this for initialization
+	void Start () {
+		pos = 0;
+		reset = false;
+		turn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!this.gameObject.animation.isPlaying) {
+			if (reset) {
+				Reset ();
+				reset = false;
+			}
+			if (turn) {
+				Turn ();
+				turn = false;
+			}
+		}
+	}
+	
+	void Reset(){
+
+
+		switch (pos)
+		{
+		case 0:
+			break;
+		case 1:
+			this.gameObject.animation.Play (reverseTurn.name);
+			pos = 0;
+			break;
+		default:
+			this.gameObject.animation.Play (turn3.name);
+			pos = 0;
+			break;
+		}
+
+	}
+
+	void Turn(){
+
+		
+		switch (pos)
+		{
+		case 0:
+			this.gameObject.animation.Play (turn1.name);
+			pos = 1;
+			break;
+		case 1:
+			this.gameObject.animation.Play (turn2.name);
+			pos = 2;
+			break;
+		default:
+			this.gameObject.animation.Play (turn3.name);
+			pos = 0;
+			break;
+		}
+		
+	}
+
+		
+		
+	
+}
