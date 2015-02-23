c836991d440820e4c61cbd863d9999738ef24584
Cheweh <joseph.pena.943@my.csun.edu>
2015-02-22 20:11:11 -0800

    stuff

---
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/BoulderTriggerZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderTriggerZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		//blah blah
+		if (other.tag == "test Ball") {
+			particleSystem.Play ();
+			Debug.Log("it hit us");
+			audio.Play();
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if (other.tag == "test Ball")
+			particleSystem.Stop ();
+	}
+}
