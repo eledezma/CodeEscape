163f6069bb69490c48d967c8762d4b2af725e8d8
AllTimePlumber <superbros2@gmail.com>
2015-03-02 11:53:07 -0800

    Level 7 Changes
    see title

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/MaxLevel7.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/MaxLevel7.cs
@@ -29,6 +29,7 @@ public class MaxLevel7 : MonoBehaviour {
 			GameObject.Find("Initialization").GetComponent<CursorTime>().showCursor=false;
 			GameObject.Find ("MaxCam").GetComponent<Camera>().depth = 1;
 			yield return new WaitForSeconds (audio.clip.length);
+			GameObject.Find ("MAX/Angel Wings/g0").GetComponent<MeshRenderer> ().enabled = true;
 			Debug.Log ("YO2");
 			this.gameObject.GetComponent<TimetoFly>().enabled = true;
 			//GameObject.Find ("Cube").GetComponent<TimetoFly> ().enabled = true;
