0d24debc4b832066f0e1dd0442b8bb90ace49a0b
AllTimePlumber <superbros2@gmail.com>
2015-03-08 17:27:32 -0700

    Changes to Stage 1 Scratch Script
    Plus changes to Stage 1 Scene

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/CamTime.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/CamTime.cs
@@ -6,7 +6,8 @@ public class CamTime : MonoBehaviour {
 	public int value;
 	// Use this for initialization
 	void Start () {
-		value = this.gameObject.GetComponent<Camera>().cullingMask;
+		//value = this.gameObject.GetComponent<Camera>().cullingMask;
+		value = this.gameObject.layer;
 	}
 	
 	// Update is called once per frame
