039ee27e170ce5c5cd6451da1a5b5e04aaf36c16
Ernie Ledezma <eledezma518@gmail.com>
2015-03-21 18:08:37 -0700

    Added Start Menu
    Only 2 options for now: Start and Quit
    Menu scene is located in Main Menu folder

---
+++ Room Stage 0/Assets/Main Menu/Scripts/GameMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour{
+bool isQuit=false;
+
+void OnMouseEnter(){
+	//change text color
+	renderer.material.color=Color.red;
+}
+
+void OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.white;
+}
+
+void OnMouseUp(){
+	//is this quit
+	if (isQuit==true) {
+		//quit the game
+		Application.Quit();
+	}
+	else {
+		//load level
+		Application.LoadLevel(1);
+	}
+}
+
+void Update(){
+	//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) {
+		Application.Quit();
+	}
+}
+}
