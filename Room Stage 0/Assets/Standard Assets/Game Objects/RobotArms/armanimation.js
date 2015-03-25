2d10a24e0315375b3da161aa57efdaa3387525e9
ol90 <oscarlugo27@gmail.com>
2015-03-25 11:57:48 -0700

    armanim
    synching anims

---
+++ Room Stage 0/Assets/Standard Assets/Game Objects/RobotArms/armanimation.js
@@ -0,0 +1,66 @@
+﻿#pragma strict
+internal var animate: Animator;
+var walking:float;
+var jackn:float;
+
+var run:float;
+
+
+
+function Start () {
+
+animate= GetComponent(Animator);
+}
+
+function Update () {
+if(Input.GetKeyDown("r")){
+run=0.1;
+}
+if(Input.GetKeyUp("r")){
+run=0.0;
+}
+if(Input.GetKeyDown("y")){
+jackn=0.1;
+}
+if(Input.GetKeyUp("y")){
+jackn=0.0;
+}
+
+if(Input.GetKeyDown("w")||Input.GetKeyDown("up")){
+		walking=0.1;
+	
+	}
+	if(Input.GetKeyUp("w")||Input.GetKeyUp("up")){
+		walking=0.0;
+	
+	}
+	if(Input.GetKeyDown("s")||Input.GetKeyDown("down")){
+		walking=0.1;
+	
+	}
+	if(Input.GetKeyUp("s")||Input.GetKeyUp("down")){
+		walking=0.0;
+	
+	}
+	if(Input.GetKeyDown("a")||Input.GetKeyDown("left")){
+		walking=0.1;
+	
+	}
+	if(Input.GetKeyUp("a")||Input.GetKeyUp("left")){
+		walking=0.0;
+	
+	}
+    if(Input.GetKeyDown("d")||Input.GetKeyDown("right")){
+		walking=0.1;
+	
+	}
+	if(Input.GetKeyUp("d")||Input.GetKeyUp("right")){
+		walking=0.0;
+	
+	}
+}
+function FixedUpdate(){
+animate.SetFloat("walking", walking);
+animate.SetFloat("run", run);
+animate.SetFloat("jackn",jackn);
+}
