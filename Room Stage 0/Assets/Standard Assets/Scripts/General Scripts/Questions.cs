3555635d266b79c4928bed1c654d32b724719d06
daniodicho <dannykaka2009@hotmail.com>
2015-04-22 22:52:27 -0700

    questions

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/Questions.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/Questions.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class Questions : MonoBehaviour {
-
+	bool answered = false;
+	int userAnswer = 0;
 	int questionNumber = 1;
 	bool guiEnabled = false;
 	bool atWall = false;
@@ -15,19 +16,19 @@ public class Questions : MonoBehaviour {
 	bool deleted = false;
 	string output = "Door is open";
 	
-	public string question1 = "Which of these is a method that can be used to print somtething on the screen?";
-	string[] answer1 = {"System.out.display()","System.out.print()","System.out.write()","System.out.displaySomethingOnScreenPlease()"};
-	public string question2 = "What's the output of the following code?\n"+
+	public static string question1 = "Which of these is a method that can be used to print somtething on the screen?";
+	static string[] answer1 = {"System.out.display()","System.out.print()","System.out.write()","System.out.displaySomethingOnScreenPlease()"};
+	public static string question2 = "What's the output of the following code?\n"+
 		"\n"+
-			"System.out.println(\"A\");"+
-			"System.out.print(\"B\")"+
-			"System.out.print(\"C\")";
-	string[] answer2 = {"ABC","A\nB\nC","A\nBC","AB\nC"};
-	public string question3 = "What class can be used to get input from the user?";
-	string[] answer3 = {"Scanner","Input","Console","Text"};
-	public string question4 = "Which of the follwing is a double?";
-	string[] answers4 = {"III","3","three","3.0"};
-	public string question5 = "If boolean1 was false and boolean2 was true, which of the the print out statements gets executed?"+
+			"System.out.println(\"A\");\n"+
+			"System.out.print(\"B\");\n"+
+			"System.out.print(\"C\");\n";
+	static string[] answer2 = {"ABC","A\nB\nC","A\nBC","AB\nC"};
+	public static string question3 = "What class can be used to get input from the user?";
+	static string[]  answer3 = {"Scanner","Input","Console","Text"};
+	public static string question4 = "Which of the follwing is a double?";
+	static string[]  answer4 = {"III","3","three","3.0"};
+	public static string question5 = "If boolean1 was false and boolean2 was true, which of the the print out statements gets executed?"+
 		"\n"+
 			"if(boolean1){\n"+
 			"\tif(boolean2){\n"+
@@ -40,25 +41,29 @@ public class Questions : MonoBehaviour {
 			"else{\n"+
 			"\tSystem.out.print(\"C\")\n"+
 			"}\n";
-	string[] answer5 = {"A","B","C","None of the above"};
-	public string question6 = "What's the output of the following code?\n"+
+	static string[] answer5 = {"A","B","C","None of the above"};
+	public static  string question6 = "What's the output of the following code?\n"+
 		"\n"+
 			"for(int i=0;i<7;i++){\n"+
 			"\tSystem.out.print(\"A\");\n"+
 			"}\n";
-	string[] answer6 = {"AAAAA","AAAAAA","AAAAAAA","AAAAAAAA"};
-	public string question7 = "What are all the possible values that the random method (int)(Math.random()*3)+2 generates?\n";
-	      string[] answer7 = {"1,2,3","2,3,4","2,3,4,5","3,4,5"};
-	public string question8 = "What's the output of the following code:"+
+	static string[] answer6 = {"AAAAA","AAAAAA","AAAAAAA","AAAAAAAA"};
+	public static string question7 = "What are all the possible values that the random method (int)(Math.random()*3)+2 generates?\n";
+	static string[]answer7 = {"1,2,3","2,3,4","2,3,4,5","3,4,5"};
+	public static string question8 = "What's the output of the following code:"+
 		"\n"+
 			"int numbers[] = {3,5,7,9};\n"+
 			"System.out.print(numbers[1]+numbers[2]);";
 
-	string[] answers8 = {"8","35","12","57"};
-	public string question9 = "Which of these is used to access a private variable from another class?";
-	string[] answers9 = {"Another variable","Set method","Key","Get method"};
-	public string question10 = "What programming language was introduced in the game?";
-	string[] answers10 = {"C++","Java","Python","Prolog"};
+	static string[] answer8 = {"8","35","12","57"};
+	public static string question9 = "Which of these is used to access a private variable from another class?";
+	static string[] answer9 = {"Another variable","Set method","Key","Get method"};
+	public static string question10 = "What programming language was introduced in the game?";
+	static string[] answer10 = {"C++","Java","Python","Prolog"};
+
+	static string[][] answers = {answer1,answer2,answer3,answer4,answer5,answer6,answer7,answer8,answer9,answer10};
+	static string[] questions = {question1,question2,question3,question4,question5,question6,question7,question8,question9,question10};
+	static int[] correctAnswers = {2,3,1,4,4,3,2,3,4,2};
 
 
 
@@ -83,7 +88,8 @@ public class Questions : MonoBehaviour {
 	}
 	
 	void Start()
-	{ //I've commented this method out and nothing bad happens :o -Joseph
+	{
+
 		Screen.lockCursor = true;
 		//Screen.showCursor = false;
 	}
@@ -91,9 +97,9 @@ public class Questions : MonoBehaviour {
 	//*******************************************************
 	void Update()
 	{
-		if (atWall)
-		{
-			if (Input.GetKeyDown("e"))
+	//	if (atWall)
+	//	{
+			if (Input.GetKeyDown("l"))
 			{
 				//			Screen.lockCursor = false;  //Cursor is free to move when user goes into terminal
 				if (guiEnabled)
@@ -106,17 +112,13 @@ public class Questions : MonoBehaviour {
 					guiEnabled = true;
 					GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled = false;
 					GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = false;
-				}
+				GameObject.Find("Initialization").GetComponent<CursorTime>().showCursor = false; 
+				Screen.lockCursor = false;
+
 			}
-		}
-		else
-		{
-			//Screen.showCursor = false;
-			//Screen.lockCursor = true;  //Hiding Cursor means redoing the way the crosshair was implemented -Josephs
-			//Screen.lockCursor = false; //Cursor remains locked if not in terminal
-		}
-		editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-		editor.MoveLineStart();
+			}
+	
+
 	}
 	//*******************************************************
 	
@@ -125,13 +127,13 @@ public class Questions : MonoBehaviour {
 	//*******************************************************
 	public void OnGUI()
 	{
-		
+		if(!answered){
 		if (!atWall)
 		{  //If not at wall terminal jack in - "show crosshair"
 			//Vector3 mPos = Input.mousePosition;
 			//GUI.DrawTexture (new Rect (mPos.x - 32, Screen.height - mPos.y - 32, 64, 64), cursorImage);
 		}
-		else if (atWall && Input.GetKeyDown("e"))
+		else if (atWall && Input.GetKeyDown("l"))
 		{
 			GameObject.Find("Initialization").GetComponent<CursorTime>().showCursor = false;
 			//If at wall terminal show default cursor instead
@@ -139,87 +141,41 @@ public class Questions : MonoBehaviour {
 		
 		if (guiEnabled)
 		{
-			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Indentation");
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "QUIZ");
 			
 			GUI.SetNextControlName("textarea");
-			GUI.TextArea(new Rect(Screen.width * 0.2f, Screen.width * 0.04f, Screen.width * 0.75f, Screen.height * 0.75f), code);
-			editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-			editor.SelectNone();
+			GUI.Label(new Rect(Screen.width*0.1f,Screen.height*0.1f,Screen.width*0.8f,Screen.height*0.5f),questions[questionNumber-1]);
 			
-			/*			GUI.Label(new Rect(500, 500, 200, 200), string.Format("Selected text: {0}\nPos: {1}\nSelect pos: {2}\nLines Before: {3}\nLines After: {4}",
-                                                      height1,
-                                                      height2,
-                                                      height3,
-                                                      countLinesBefore(code,editor.pos),
-                                                      height4));*/
+
 			
 			
 			// Button that inserts a tab 
-			if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.18f, Screen.width * 0.1f, Screen.height * 0.05f), "Add Tab"))
+			if (GUI.Button(new Rect(Screen.width * 0.3f, Screen.height * 0.6f, Screen.width * 0.25f, Screen.height * 0.2f), answers[questionNumber-1][0]))
 			{
-				GUI.FocusControl("textarea");
-				editor = goToNextEmptyLine(editor, code);
-				//if((countLinesBefore(code,editor.pos)>=2)&&(countLinesAfter(code,editor.pos)>=2)&&isBlankLine(code,edi
-				code = addToCode(code, editor, "\t");
-				if (added)
+					userAnswer = 1;
+				
+			}
+			
+			if (GUI.Button(new Rect(Screen.width * 0.6f, Screen.height * 0.6f, Screen.width * 0.25f, Screen.height * 0.2f), answers[questionNumber-1][1]))
+			{
+					userAnswer=2;
+			}
+
+			if (GUI.Button(new Rect(Screen.width * 0.3f, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.2f), answers[questionNumber-1][2]))
 				{
-
+					userAnswer=3;
+					
 				}
-			}
-			
-			// Button that inserts a for loop
-			if (GUI.Button(new Rect(Screen.width * 0.02f, Screen.height * 0.28f, Screen.width * 0.1f, Screen.height * 0.05f), "Erase Tab"))
-			{
-				GUI.FocusControl("textarea");
-				editor = goToNextEmptyLine(editor, code);
-				code = deleteFromCode(code, editor);
-				if (deleted)
+				
+			if (GUI.Button(new Rect(Screen.width * 0.6f, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.2f), answers[questionNumber-1][3]))
 				{
-				
+					userAnswer =4;
 				}
-			}
 			
 			
-			// Button that activates the user's code
-			if (GUI.Button(new Rect(Screen.width * 0.6f, Screen.height * 0.9f, Screen.width * 0.08f, Screen.height * 0.05f), "Submit"))
-			{
-				TextChanger.Update();
-
-				
-				int cube1 = GameObject.Find("Cube1").GetComponent<MoveBlockStage3>().height;
-				int cube2 = GameObject.Find("Cube2").GetComponent<MoveBlockStage3>().height;
-				int cube3 = GameObject.Find("Cube3").GetComponent<MoveBlockStage3>().height;
-				int cube4 = GameObject.Find("Cube4").GetComponent<MoveBlockStage3>().height;
-				int cube5 = GameObject.Find("Cube5").GetComponent<MoveBlockStage3>().height;
-				int cube6 = GameObject.Find("Cube6").GetComponent<MoveBlockStage3>().height;
-				int cube7 = GameObject.Find("Cube7").GetComponent<MoveBlockStage3>().height;
-				
-				if ((cube1 == 0) && (cube2 == -1) && (cube3 == -2) && (cube4 == -3) && (cube5 == -2) && (cube6 == -1) && (cube7 == 0))
-					audio.PlayOneShot(missionComplete);
-				
-
-				resume();
-			}
 			
-			// Button that closes the UI and disregards changes
-			if (GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.9f, Screen.width * 0.08f, Screen.height * 0.05f), "Cancel"))
-			{
-				resume();
-			}
 			
-			// Button that restores the code in the textArea to its original state
-			if (GUI.Button(new Rect(Screen.width * 0.8f, Screen.height * 0.9f, Screen.width * 0.08f, Screen.height * 0.05f), "Reset"))
-			{
-				code = restoreCode();
-
-				GameObject.Find("Cube1").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube2").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube3").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube4").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube5").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube6").GetComponent<MoveBlockStage3>().height = 0;
-				GameObject.Find("Cube7").GetComponent<MoveBlockStage3>().height = 0;
-				
+			
 			}
 		}
 	}
@@ -373,5 +329,5 @@ public class Questions : MonoBehaviour {
 		GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled = true;
 		GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = true;
 	}
-	
+
 }
