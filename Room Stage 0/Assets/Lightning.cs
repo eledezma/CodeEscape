ece049649a39a8584bd424c83e97d33c3e1fa23b
Cheweh <joseph.pena.943@my.csun.edu>
2015-04-26 14:53:35 -0700

    2sick2care
    I opted for the cheap lightning, LASERS

---
+++ Room Stage 0/Assets/Lightning.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour {
+
+	private LineRenderer lineRenderer;
+	public Transform origin;
+	public Transform dest;
+
+	
+	void Start () { //I just went with laser beams....
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.enabled = false;
+		lineRenderer.SetPosition(0, origin.position);
+		lineRenderer.SetPosition(1, dest.position);
+	}
+
+//	void Start () {  //problems with cool looking lightning, I'll leave if anyone want to mess, dig?
+//		lineRenderer = GetComponent<LineRenderer>();
+//		lineRenderer.SetVertexCount(numOfSegments);
+//		for(int i=1; i < vertices-1; i++)
+//		{
+//			float y =((float)i)*(maxy)/(float)(numOfSegments-1);
+//			lineRenderer.SetPosition(i, new Vector3(Random.Range([rightVec],-[rightVec]),y,Random.Range([atVec],-[atVec])));
+//		}
+//		lineRenderer.SetPosition(0, origin.position);
+//		lineRenderer.SetPosition(vertices-1, dest.position);
+		//destroy on update, redraw random path to end position
+//	}
+
+
+
+	void Update () {  //could add sound for shut off but...don't know which node you guys want sound on
+	if (Input.GetKeyDown("4") && lineRenderer.enabled == false)
+		lineRenderer.enabled = true;
+	else if(Input.GetKeyDown("4") && lineRenderer.enabled == true)
+		lineRenderer.enabled = false;
+	}
+
+}
