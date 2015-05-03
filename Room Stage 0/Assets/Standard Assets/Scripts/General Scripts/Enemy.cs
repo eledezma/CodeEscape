21c935c20497638d83cd39c8d256cb5f9f6783ab
daniodicho <dannykaka2009@hotmail.com>
2015-05-02 20:56:43 -0700

    fix

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/Enemy.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/Enemy.cs
@@ -20,15 +20,14 @@ public class Enemy : MonoBehaviour
 	private Vector3 Target;
 	private GameObject go;
 	private GameObject enemy;
-	private float time = 2000;
 	private bool attacking = false;
 	private bool pushedBack = false;
 	private AnimationClip Walk;
 	private float distToGround;
 	private Transform myTransform;
-	private float health = 100;
-	private GameObject bulletShot;
-
+	public float health = 100;
+	public GameObject bulletShot;
+	
 	
 	// Use this for initialization
 	void Start()
@@ -47,107 +46,107 @@ public class Enemy : MonoBehaviour
 	void Update()
 	{
 		distance = Vector3.Distance(player.position,myTransform.position);
-
+		
 		if(myTransform.position.y<0.5f)
 			myTransform.Translate(0,0.05f,0);
-
+		
 		if(!pushedBack&&IsGrounded())
 		{
-		switch (state)
-		{
-		case "patrol":
-		{
-			speed = originalSpeed;
-				
-			if(curWayPoint<waypoints.Length)
+			switch (state)
 			{
-				//Target = new Vector3(waypoints[curWayPoint].position.x,1,waypoints[curWayPoint].position.z);
-					Target = waypoints[curWayPoint].position;
-
-				MoveDirection = Target - myTransform.position;
-				Velocity = rigidbody.velocity;
-				if(MoveDirection.magnitude <1)
+			case "patrol":
+			{
+				speed = originalSpeed;
+				
+				if(curWayPoint<waypoints.Length)
 				{
-					curWayPoint++;
+					//Target = new Vector3(waypoints[curWayPoint].position.x,1,waypoints[curWayPoint].position.z);
+					Target = waypoints[curWayPoint].position;
+					
+					MoveDirection = Target - myTransform.position;
+					Velocity = rigidbody.velocity;
+					if(MoveDirection.magnitude <1)
+					{
+						curWayPoint++;
+					}
+					else
+					{
+						Velocity = MoveDirection.normalized * speed;		
+					}
 				}
 				else
 				{
-					Velocity = MoveDirection.normalized * speed;		
+					curWayPoint = 0;
 				}
+				rigidbody.velocity = Velocity;
+				myTransform.LookAt(Target);
 			}
-			else
+				break;
+			case "chasing":
 			{
-				curWayPoint = 0;
+				
+				speed = originalSpeed;
+				Target = new Vector3(player.position.x,1,player.position.z);
+				MoveDirection = Target - myTransform.position;
+				Velocity = MoveDirection.normalized * speed;
+				rigidbody.velocity = Velocity;
+				myTransform.LookAt(Target);
+				enemy.animation.Play("Run");
 			}
-			rigidbody.velocity = Velocity;
-			myTransform.LookAt(Target);
-		}
-			break;
-		case "chasing":
-		{
-			
-			speed = originalSpeed;
-			Target = new Vector3(player.position.x,1,player.position.z);
-			MoveDirection = Target - myTransform.position;
-			Velocity = MoveDirection.normalized * speed;
-			rigidbody.velocity = Velocity;
-			myTransform.LookAt(Target);
-			enemy.animation.Play("Run");
-		}
-			break;
-		case "attacking":
-		{
-			speed = 0;
-			Velocity = MoveDirection.normalized * speed;
-			rigidbody.velocity = Velocity;
-			enemy.animation.Play("Attack");
-			
-			timer-=Time.deltaTime;
-
-			if(timer<0){
-				timer=1.25f;
-				go.GetComponent<Player>().Health-=25;
+				break;
+			case "attacking":
+			{
+				speed = 0;
+				Velocity = MoveDirection.normalized * speed;
+				rigidbody.velocity = Velocity;
+				enemy.animation.Play("Attack");
+				
+				timer-=Time.deltaTime;
+				
+				if(timer<0){
+					timer=1.25f;
+					go.GetComponent<Player>().Health-=25;
+				}
+				
+				
 			}
-			
-					
-		}
-			break;
-		case "repelling":
-		{
-			MoveDirection = Target + myTransform.position;
-			attacking = false;
-		}
-			break;
-		case "dead":
-		{
-			enemy.animation.Play("Death");
+				break;
+			case "repelling":
+			{
+				MoveDirection = Target + myTransform.position;
+				attacking = false;
+			}
+				break;
+			case "dead":
+			{
+				enemy.animation.Play("Death");
 				Velocity*=0;
 				MoveDirection*=0;
 				GetComponent<Enemy>().enabled=false;
+			}
+				break;
+			}
+			if(distance<attackRange)
+			{
+				state = "attacking";
+			}
+			
+			else if(distance<detectionRange&&(distance>attackRange))
+			{
+				state = "chasing";
+			}
+			else
+			{
+				state = "patrol";
+			}
+			
 		}
-			break;
-	}
-		if(distance<attackRange)
-		{
-			state = "attacking";
-		}
-
-		else if(distance<detectionRange&&(distance>attackRange))
-		{
-			state = "chasing";
-		}
-		else
-		{
-			state = "patrol";
-		}
-
-	}
 		else if(distance > 15)
 		{
 			pushedBack = false;
 			state = "chasing";
 		}
-
+		
 		if(Input.GetKeyDown("e")&&(distance<pushRange))
 		{
 			speed = originalSpeed;
@@ -157,7 +156,7 @@ public class Enemy : MonoBehaviour
 			rigidbody.velocity = Velocity;
 			attacking=false;
 		}
-
+		
 		if(bulletShot!= null && Vector3.Distance(bulletShot.transform.position,transform.position)<1.0f)
 		{
 			health-=50;
@@ -168,21 +167,21 @@ public class Enemy : MonoBehaviour
 		}
 		
 	}
-
+	
 	public bool IsGrounded(){
 		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.5f);
 	}
-
+	
 	public bool PushedBack{
 		get{ return pushedBack; }
 		set{ pushedBack = value; }
 	}
-
+	
 	public string State{
 		get{ return state; }
 		set{ state = value; }
 	}
-
+	
 	public GameObject BulletShot{
 		get{ return bulletShot; }
 		set{ bulletShot = value; }
