ca6a05a39f6bd82ad07e3590d04ae64a4ce46623
818headbanger <josepech818@gmail.com>
2015-05-06 12:02:17 -0700

    Level 1 Scanner Info
    see title

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ScannerInfoLevel1.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ScannerInfoLevel1.cs
@@ -5,11 +5,22 @@ public class ScannerInfoLevel1 : MonoBehaviour
 {
 
 	public bool guiEnabeled = true;
-	private string info = "The Scanner class is part of the java.util package." +
-		"\nScanner's in java are used to get user input, but" +
-			"\n that is not their only purpose. The method nextLine()" +
-			"\n is part of the Scanner class and is used to get the " +
-			"\n next line the user enters as the input.";
+	private string info = "If you want to print data to the screen, two of the methods available" +
+						  "\nin java are the System.out.print() and System.out.println(). If you use the print() method it will " +
+						  "\nprint the variables with out making a new line. For example:" +
+			"\n\n\t int i = 1;" +
+			"\n\tSystem.out.print("+"This is a example using the number: "+");"+
+			"\n\tSystem.out.print(i);" +
+			"\n\nThe output for this example will be" +
+			"\nThis is a example using the number: 1" +
+			"\n\nIf we use the prinln() method our output will be different, for example:" +
+			"\n\n\t int i = 1;" +
+			"\n\tSysytem.out.println("+"This is a example using the number: "+");" +
+			"\n\tSystem.out.println(i);" +
+			"\n\nThe output for this example will be " +
+			"\nThis is a example using the number:" +
+			"\n1" +
+			"\n\n\n\nPress E to continue";
 	// Use this for initialization
 	void Start()
 	{
