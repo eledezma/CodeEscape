d308d3717dba2d534ac4d03cd762658e40c37ed5
daniodicho <dannykaka2009@hotmail.com>
2015-05-07 16:48:56 -0700

    brushing up

--- Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ScannerInfoLevel8.cs
+++ Room Stage 0/Assets/Standard Assets/Scripts/General Scripts/ScannerInfoLevel8.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class ScannerInfoLevel8 : MonoBehaviour 
 {
-
 	public bool guiEnabeled = true;
 	private string info ="Arrays are containers that hold a fixed number of values of a single type. The length of" +
-		"\nthe array is assigned when the aray is created, and the length is fixed one created. Each item in the array is called an" +
-			"\nelement and it can be accessed with their numerical index. The index ranges from a value of 0 to n-1, where n " +
-			"\nis the number of items. This means that the first item will be in index 0 and the nth item will be in index n-1. In Java, there" +
-			"\n an Array Class that can help get useful information about arrays, or manipulate arrays. For example the length method would tell you the " +
-			"\nthe size if the array and the copyFromRange method lets you copy item from a certain range.";
+		" the array is assigned when the aray is created, and the length is fixed one created. Each item in the array is called an" +
+			" element and it can be accessed with their numerical index. The index ranges from a value of 0 to n-1, where n " +
+			" is the number of items. This means that the first item will be in index 0 and the nth item will be in index n-1. In Java, there" +
+			"  an Array Class that can help get useful information about arrays, or manipulate arrays. For example the length method would tell you the " +
+			" the size if the array and the copyFromRange method lets you copy item from a certain range."+
+			"\n\nPress E to continue...";
+
 	// Use this for initialization
 	void Start()
 	{
@@ -34,7 +35,8 @@ public class ScannerInfoLevel8 : MonoBehaviour
 		if (guiEnabeled)
 		{
 			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-			GUI.Label(new Rect(Screen.width * 0.45f, Screen.height * 0.01f, Screen.width * 0.1f, Screen.height * 0.05f),"Info");
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+			GUI.Label(new Rect(Screen.width * 0.45f, Screen.height * 0.01f, Screen.width * 0.1f, Screen.height * 0.05f),"Arrays");
 			GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.8f, Screen.height * 0.8f),info);
 			if (Input.GetKeyDown("e"))
 			{
